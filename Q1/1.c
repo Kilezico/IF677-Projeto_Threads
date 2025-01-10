6c68249166162a3ceb7990fa3a6c1135ad01550a
Bolotinhas <156836553+Bolotinhas@users.noreply.github.com>
2025-01-10 09:54:39 -0300

    Questão 1

--- Q1/1.c
+++ Q1/1.c
@@ -7,21 +7,20 @@ char palavra[100]; //variavel global contendo a palavra
 int d = 0, e = 0;//variaveis globais onde d contem o numero de arquivos terminados, e controla a quantidade de threads ativas
 
 void* Procurar(void* search){
-FILE* file; //criando variaveis
-file = fopen((char*) search, "r"); //leitura do arquivo
-int f = 0, i = 0; //f representa a linha atual do arquivo e i representa o indice da palavra
-char frase[200];//variavel da frase
-int cond = 0; //condicao para checar se as letras sao iguais
+FILE* file; //criando variavel para leitura do arquivo
+file = fopen((char*) search, "r"); 
+int f = 0, i = 0, cond = 0; //f representa a linha atual do arquivo, i representa o indice da palavra, cond representa uma flag para analisar se as letras sao iguais 
+char frase[200]; //variavel para ler as frases de cada linha do arquivo txt
+    
 while(fgets(frase, 200, file) != NULL){ //loop para coletar as linhas do arquivo
-f++; //a linha atual e atualizada
-for(int c = 0; c < strlen(frase); c++){ //loop para chegar ate o final da linha passando letra por letra
-if(!cond){i = 0;} else{i++;} //se a condicao for 1, o indice i e incrementado, e caso contrario, o indice i e zerado
-if(i == strlen(palavra)){printf("<%s>:<%d>\n",(char*) search,f); i = 0; cond = 0; continue;} //se a palavra for encontrada, sera printado o nome do arquivo e a linha, e o loop sera continuado a partir da proxima iteracao
-if(frase[c] == palavra[i]){cond = 1;} else{cond = 0;} //se as letras forem iguais, a condicao e 1
+ f++; //a linha atual e atualizada
+ for(int c = 0; c < strlen(frase); c++){ //loop para chegar ate o final da linha passando letra por letra
+      if(frase[c] == palavra[i]){cond = 1;} else{cond = 0;} //se as letras forem iguais, a condicao e 1
+      if(!cond){i = 0;} else{i++;} //se a condicao for 1, o indice i e incrementado, e caso contrario, o indice i e zerado
+      if(i == strlen(palavra)){printf("<%s>:<%d>\n",(char*) search,f); i = 0; cond = 0;} //se a palavra for encontrada, sera printado o nome do arquivo e a linha 
+  }
 }
-}
-d++; //numeros de arquivos finalizados aumenta
-e--; // numero de threads disponiveis aumenta
+d++; e--; //numeros de arquivos finalizados aumenta e numero de threads disponiveis aumenta
 pthread_exit(NULL);
 }
 
@@ -39,11 +38,10 @@ int main()
     }
     //threads
     pthread_t *threads = (pthread_t*) calloc(thread_number, sizeof(pthread_t));
-   //caso haja mais threads disponiveis do que arquivos
-   if(thread_number > file_number){thread_number = file_number;}
+    if(thread_number > file_number){thread_number = file_number;} //caso haja mais threads disponiveis do que arquivos, os numeros de threads serao iguais ao numero de arquivos
    while(d < file_number){//loop que terminara somente quando todos arquivos forem lidos
-    for(; (e < thread_number) && (c < file_number); e++){ //for que sempre ativara quando houver threads disponiveis
-       b = pthread_create(&threads[e], NULL, Procurar, (void*) nomes[c]); c++; //criando threads e atualizando a variavel c para ser usado o proximo arquivo
+    for(; (e < thread_number) && (c < file_number); e++){ //loop que sempre ativara quando houver threads disponiveis e arquivos que nao foram analisados
+       b = pthread_create(&threads[e], NULL, Procurar, (void*) nomes[c]); c++; //criando threads para ler arquivos nao lidos e atualizando a variavel c para a proxima thread usar o proximo arquivo
         if(b){ //checando se houve erro nas threads
             printf("Erro e o retorno e %d\n", b);
             exit(-1);
