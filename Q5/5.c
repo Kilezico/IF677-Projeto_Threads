e3eb394d9eacb071137efbc534f6a24a316388ab
Bolotinhas <156836553+Bolotinhas@users.noreply.github.com>
2025-01-28 08:56:57 -0300

    despachante começou

--- Q5/5.c
+++ Q5/5.c
@@ -23,6 +23,8 @@ typedef struct {
 } TempElem;
 
 pthread_t threads_ativas[N];
+pthread_mutex_t mutex;
+pthread_cond_t cond;
 // Buffer de execuções pendentes de funções
 BuffElem buffer[BUFFER_SIZE];
 int items = 0;
@@ -51,7 +53,15 @@ int executora(int idc, int (*funexec)(void *), void *args) // Executa a função
 
 void *despachante(void *arg) // Gerencia a criação de threads para executar funções funexec
 {
-
+   int rc = 0;
+    while(1){
+        pthread_mutex_lock(&mutex);
+        while(items == 0){
+        pthread_cond_wait(&cond, &mutex);    
+        }
+        
+        
+    }  
 }
 
 int pegarResultadoExecucao(int id) // Recebe um id e bloqueia a thread até obter o resultado
