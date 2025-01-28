32404936307b310b74ae8a185602ef5924dd3ad1
Kilezico <jgrangeirocosta@gmail.com>
2025-01-28 18:51:39 -0300

    pegarResultadoExecucao preenchida

--- Q5/5.c
+++ Q5/5.c
@@ -18,7 +18,8 @@ typedef struct {
 
 typedef struct {
     int id;
-    int finished;
+    int finished; // FLAG - terminou ou não execução
+    int delivered; // FLAG - recebeu ou não resultado
     pthread_cond_t cond_finished;
     int value;
 } TempElem;
@@ -35,7 +36,7 @@ int last = 0;
 int fios_ativos = 0;
 // Buffer temporario
 TempElem temp[TEMP_SIZE];
-int prox_id = 1;
+pthread_mutex_t temp_mutex = PTHREAD_MUTEX_INITIALIZER; // Mutex que controla o acesso ao buffer temporário
 
 // Funções
 int agendarExecucao(int (*funexec)(void *), void *args) // Recebe função e argumentos e retorna ID para depois receber resultado
@@ -73,7 +74,24 @@ void *despachante(void *arg) // Gerencia a criação de threads para executar fu
 
 int pegarResultadoExecucao(int id) // Recebe um id e bloqueia a thread até obter o resultado
 {
+    // Procura o ID no vetor de resultados
+    int retorno;
+    for (int i=0; i<TEMP_SIZE; i++) {
+        if (temp[i].id == id) { // Achou o ID
+            pthread_mutex_lock(&temp_mutex);
+            while (temp[i].finished == 0) { // Dorme até terminar de executar
+                pthread_cond_wait(&temp[i].cond_finished, &temp_mutex);
+            }
 
+            retorno = temp[i].value;
+            temp[i].delivered = 1; // Marca como recebido
+            pthread_cond_destroy(&temp[i].cond_finished); // Libera memória da variável de condição
+            pthread_mutex_unlock(&temp_mutex);
+        
+            return retorno;
+        }
+    }
+    return -1; // Se o id recebido existe, nunca chegará aqui.
 }
 
 int mein()
